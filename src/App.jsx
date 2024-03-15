8068d2e6d6fe57e5b361623448aacbb6e15c309b
Parmar Vishal Karmshibhai <123800206+parmarvishal31@users.noreply.github.com>
2024-03-15 22:22:47 +0530

    hader change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Headers from "./components/Header";
+import Header from "./components/Header"
 
  function App() {
   return (
     <div>
-      <Headers />
+      <Header />
+      <h1></h1>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,205 +1,115 @@
-import { Fragment, useState } from 'react'
-import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
+import React from 'react'
+import { useState, useEffect } from "react";
+import { Menu, X } from "lucide-react";
+import { AnimatePresence, motion } from "framer-motion";
 import {
-  ArrowPathIcon,
-  Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+  mobileNavContainerVariant,
+  mobileNavListVariant,
+  mobileNavExitProps,
+} from "../data/animationConfig";
+import { NavLink, useLocation } from 'react-router-dom';
 import Logo from '../assets/RTAC_Logo_Without _Border.png'
+import { FaPhoneVolume } from "react-icons/fa6";
 
-const products = [
-  { name: 'MS-Office', href: '/courses'},
-  { name: 'Tally', href: '/courses'},
-  { name: 'CCC', href: '/courses'},
-  { name: 'DTP', href: '/courses'},
-  { name: 'Auto-CAd', href: '/courses'},
-  { name: 'PGDCA', href: '/courses'},
-  { name: 'Hardware', href: '/courses'},
-  { name: 'Networking', href: '/courses'},
-  { name: 'Web-design', href: '/courses'},
-  { name: 'Mobile Repairing', href: '/courses'},
-  { name: 'Mobile Beauty Parlor', href: '/courses'},
+  const activeClassName = "text-red-400 me-2";
+const activeStyleCallback = ({ isActive }) =>
+  isActive ? activeClassName : " me-2 hover:text-red-400 ";
 
-]
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
-]
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
-export default function Headers() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-
-  // Function to handle closing the mobile menu
-  const closeMobileMenu = () => {
-    setMobileMenuOpen(false)
-  }
-
+const NavLinks = () => {
   return (
-    <header className="bg-white border-b-2 ">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-2 lg:px-8" aria-label="Global">
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5 flex justify-center items-center gap-1">
-            <img className="h-14 w-auto" src={Logo} alt="Logo" />
-          </a>
-        </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
+    <>
+      <NavLink to="/" className={activeStyleCallback}>
+        Home
+      </NavLink>
 
-        <Popover.Group className="hidden lg:flex lg:gap-x-12">
-          <a href="#" className="text-sm font-semibold leading-6 text-red-500">
-            Home
-          </a>
-          <Popover className="relative">
-            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-              Courses
-              <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-            </Popover.Button>
-
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-56  rounded-3xl h-96 overflow-auto bg-white shadow-lg ring-1 ring-gray-900/5">
-                <div className="p-4">
-                  {products.map((item) => (
-                    <div
-                      key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-2 text-sm leading-6 hover:bg-gray-50"
-                    >
-                      <div className="flex-auto">
-                        <a href={item.href} className="block font-semibold text-gray-900">
-                          {item.name}
-                          <span className="absolute inset-0" />
-                        </a>
-    
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                
-              </Popover.Panel>
-            </Transition>
-          </Popover>
-
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            About us
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Contact us
-          </a>
-        
-        </Popover.Group>
-      </nav>
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between border-b-2 pb-3">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-12 w-auto"
-                src={Logo}
-                alt="logo"
-              />
-            </a>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
+      <NavLink to="/courses" className={activeStyleCallback}>
+        Courses
+      </NavLink>
+      <NavLink to="/about" className={activeStyleCallback}>
+        About us
+      </NavLink>
+      <NavLink to="/contact" className={activeStyleCallback}>
+        Contact us
+      </NavLink>
+    </>
+  );
+};
+const Nav = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const location = useLocation();
+  
+    const toggleNavbar = () => {
+      setIsOpen(!isOpen);
+    };
+  
+    useEffect(() => {
+      setIsOpen(false);
+    }, [location.pathname]);
+  
+    return (
+      <>
+        <nav className="flex flex-[1] items-center justify-end overflow-hidden">
+          <div className="hidden justify-end md:flex">
+            <NavLinks />
           </div>
-          <div className=" flow-root mt-0">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-              
-                <Disclosure as="div" className="-mx-3">
-                  {({ open }) => (
-                    <>
-                    <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 ml-[1.5px] text-base font-semibold leading-7 text-red-500  hover:bg-gray-50"
-                  onClick={closeMobileMenu}
-                >
+          <div className="w-[75px]">
+            {/* <ThemeToggle /> */}
+          </div>
+          <div className="flex w-[75px] justify-end md:hidden">
+            <button onClick={toggleNavbar}>{isOpen ? <X /> : <Menu />}</button>
+          </div>
+        </nav>
+        <AnimatePresence mode="wait">
+          {isOpen && (
+            <motion.div
+              layout="position"
+              key="nav-links"
+              variants={mobileNavContainerVariant}
+              initial="hidden"
+              animate="show"
+              className="mt-4 basis-full md:hidden"
+            >
+              <motion.div variants={mobileNavListVariant} {...mobileNavExitProps}>
+                <NavLink to="/" className={activeStyleCallback}>
                   Home
-                </a>
-                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                      Courses
-                        <ChevronDownIcon
-                          className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
-                          aria-hidden="true"
-                        />
-                      </Disclosure.Button>
-                      <Disclosure.Panel className="mt-2 space-y-2">
-                        {[...products, ...callsToAction].map((item) => (
-                          <Disclosure.Button
-                            key={item.name}
-                            as="a"
-                            href={item.href}
-                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                            onClick={closeMobileMenu} // Close the mobile menu when a link is clicked
-                          >
-                            {item.name}
-                          </Disclosure.Button>
-                        ))}
-                      </Disclosure.Panel>
-                    </>
-                  )}
-                </Disclosure>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  onClick={closeMobileMenu}
-                >
+                </NavLink>
+              </motion.div>
+              <motion.div variants={mobileNavListVariant} {...mobileNavExitProps}>
+                <NavLink to="/courses" className={activeStyleCallback}>
+                  Courses
+                </NavLink>
+              </motion.div>
+              <motion.div variants={mobileNavListVariant} {...mobileNavExitProps}>
+                <NavLink to="/about" className={activeStyleCallback}>
                   About us
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  onClick={closeMobileMenu}
-
-                >
+                </NavLink>
+              </motion.div>
+              <motion.div variants={mobileNavListVariant} {...mobileNavExitProps}>
+                <NavLink to="/contact" className={activeStyleCallback}>
                   Contact us
-                </a>
-                
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  onClick={closeMobileMenu} // Close the mobile menu when a link is clicked
-                >
-                  Phone number
-                </a>
-              </div>
-            </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
-    </header>
+                </NavLink>
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </>
+    );
+  };
+  
+
+function Header() {
+  return (
+    <header className="sticky top-0 z-[1]   flex w-full px-4 py-2 flex-wrap items-center justify-between border-b  bg-background  font-sans font-semibold  text-text-primary backdrop-blur-[100px] " style={{boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px"}}>
+     <NavLink to="/" className="inline-block">
+        <img
+          alt="Blog Logo"
+          src={Logo}
+          className="block w-[65px] md:ml-10"
+        />
+      </NavLink>
+    <Nav />
+  </header>
   )
 }
+
+export default Header
