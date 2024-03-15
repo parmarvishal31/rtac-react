c4c72eb7444db9b8d6f36e07803add6339915416
Parmar Vishal Karmshibhai <123800206+parmarvishal31@users.noreply.github.com>
2024-03-16 00:44:20 +0530

    bug fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <div className='flex w-full justify-center items-center rounded-t-lg'>
         <div className='bg-gray-700 w-full flex justify-center items-center gap-1 font-semibold border-r-2 border-black text-white'><span className='text-green-700'><IoLogoWhatsapp /></span><span>Whatsapp</span></div>
-        <div className='bg-gray-600 w-full flex justify-center items-center gap-1 font-semibold border-r-2 border-black text-white'><span className='text-red-500'><PiPhoneCallFill /></span><span>Call Now</span></div>
+        <div className=' w-full flex justify-center items-center gap-1 font-semibold border-r-2 border-black text-white bg-gray-700'><span className='text-red-500'><PiPhoneCallFill /></span><span>Call Now</span></div>
         <div className='bg-gray-700 w-full flex justify-center items-center gap-1 font-semibold text-white'><span className='text-yellow-500'><MdEmail /></span><span>Mail</span></div>
     </div>
   )
