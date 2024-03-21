37e83068fc2e37a67497af0270919bf09d0d719a
Parmar Vishal Karmshibhai <123800206+parmarvishal31@users.noreply.github.com>
2024-03-21 21:27:58 +0530

    link add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,27 +2,43 @@ import React from "react";
 import { IoLogoWhatsapp } from "react-icons/io";
 import { PiPhoneCallFill } from "react-icons/pi";
 import { MdEmail } from "react-icons/md";
+import { Link } from "react-router-dom";
 function Footer() {
+  const phoneNumber = "+917778881058";
+  const email = "mailto:rtacedu@gmail.com";
+  const whatsup = "https://api.whatsapp.com/send?phone=7778881058";
+
   return (
     <div className="flex w-full justify-center items-center rounded-t-lg">
-      <div className="bg-gray-700 w-full p-2 flex justify-center items-center gap-1 font-semibold border-r-2 border-black text-white">
+      <Link
+        to={whatsup}
+        className="bg-gray-700 w-full p-2 flex justify-center items-center gap-1 font-semibold border-r-2 border-black text-white"
+      >
         <span className="text-green-700">
           <IoLogoWhatsapp />
         </span>
         <span>Whatsapp</span>
-      </div>
-      <div className=" w-full p-2 flex justify-center items-center gap-1 font-semibold border-r-2 border-black text-white bg-gray-700">
+      </Link>
+
+      <Link
+        to={`tel:${phoneNumber}`}
+        className=" w-full p-2 flex justify-center items-center gap-1 font-semibold border-r-2 border-black text-white bg-gray-700"
+      >
         <span className="text-red-500">
           <PiPhoneCallFill />
         </span>
         <span>Call </span>
-      </div>
-      <div className="bg-gray-700 w-full p-2 flex justify-center items-center gap-1 font-semibold text-white">
+      </Link>
+
+      <Link
+        to={email}
+        className="bg-gray-700 w-full p-2 flex justify-center items-center gap-1 font-semibold text-white"
+      >
         <span className="text-yellow-500">
           <MdEmail />
         </span>
         <span>Mail</span>
-      </div>
+      </Link>
     </div>
   );
 }
